aa9584b3771355655909adf38f85a085c30af069
werrolf <rolf.neuberger@wheregroup.com>
2021-07-06 14:48:54 +0200

    Fix bundle build not tracking config changes

--- MapbenderDataManagerBundle.php
+++ MapbenderDataManagerBundle.php
@@ -3,6 +3,7 @@ namespace Mapbender\DataManagerBundle;
 
 use Mapbender\CoreBundle\Component\MapbenderBundle;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
@@ -29,5 +30,6 @@ class MapbenderDataManagerBundle extends MapbenderBundle
         $configLocator = new FileLocator(__DIR__ . '/Resources/config');
         $loader = new XmlFileLoader($container, $configLocator);
         $loader->load('services.xml');
+        $container->addResource(new FileResource($loader->getLocator()->locate('services.xml')));
     }
 }
