526845e003eb539570b3518d1218d6627019e118
werrolf <rolf.neuberger@wheregroup.com>
2021-07-14 13:59:29 +0200

    Undo SchemaFilterLegacy registry replacement hack (no longer invoked with registryy argument)

--- Component/SchemaFilterLegacy.php
+++ Component/SchemaFilterLegacy.php
@@ -4,21 +4,6 @@
 namespace Mapbender\DataManagerBundle\Component;
 
 
-use Mapbender\DataSourceBundle\Component\DataStoreService;
-
 class SchemaFilterLegacy extends SchemaFilter
 {
-    /**
-     * @param mixed[][] $schemaConfigs
-     * @param DataStoreService|null $registry
-     * @return mixed[][]
-     */
-    public function prepareConfigs($schemaConfigs, DataStoreService $registry = null)
-    {
-        $registryBefore = $this->registry;
-        $this->registry = $registry ?: $this->registry;
-        $rv = parent::prepareConfigs($schemaConfigs);
-        $this->registry = $registryBefore;
-        return $rv;
-    }
 }
