4825436b735bc252ce4981f685e618fc7ff4c17b
werrolf <rolf.neuberger@wheregroup.com>
2021-08-06 10:06:25 +0200

    Require element service API; remove legacy Element classes

--- MapbenderDataManagerBundle.php
+++ MapbenderDataManagerBundle.php
@@ -1,33 +1,19 @@
 <?php
 namespace Mapbender\DataManagerBundle;
 
-use Mapbender\CoreBundle\Component\MapbenderBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Data manager bundle.
  * 
  * @author Andriy Oblivantsev
  */
-class MapbenderDataManagerBundle extends MapbenderBundle
+class MapbenderDataManagerBundle extends Bundle
 {
-    /**
-     * @inheritdoc
-     */
-    public function getElements()
-    {
-        if ($this->useService()) {
-            return array();
-        } else {
-            return array(
-                'Mapbender\DataManagerBundle\Element\DataManagerElement',
-            );
-        }
-    }
-
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
@@ -35,19 +21,12 @@ class MapbenderDataManagerBundle extends MapbenderBundle
         $loader = new XmlFileLoader($container, $configLocator);
         $loader->load('services.xml');
         $container->addResource(new FileResource($loader->getLocator()->locate('services.xml')));
-        if ($this->useService()) {
-            $loader->load('elements.xml');
-            $container->addResource(new FileResource($loader->getLocator()->locate('elements.xml')));
-        }
+        $loader->load('elements.xml');
+        $container->addResource(new FileResource($loader->getLocator()->locate('elements.xml')));
     }
 
-    protected static function useService()
+    public function getContainerExtension()
     {
-        try {
-            return \class_exists('\Mapbender\Component\Element\AbstractElementService');
-        } catch (\ErrorException $e) {
-            // Thrown by Symfony 3+ debug class loader
-            return false;
-        }
+        return null;
     }
 }
